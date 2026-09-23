a0889bf4090089a4c4245861f252d2cd15964463
Alejandro Chávez <alejandroacccollege@gmail.com>
2026-09-22 21:54:34 -0600

    Change the validation of the recived messages

--- 17evaluacion.c
+++ 17evaluacion.c
@@ -33,7 +33,7 @@ void countMessages(Message *messaggeInserted, int N, char *userName, int *sent, 
             }
         }
     }// Validates if the user has a succesful message sent
-    if ((messagePointer->status == DELIVERED)){
+    if ((strcmp(userName, messagePointer->sender) != 0) && (messagePointer->status == DELIVERED)){
         for(int j = 0; j < messagePointer->destinationCount; j++){
             if ((strcmp(userName, *(messagePointer->destination + j)) == 0)){
                 (*recived)++;
@@ -132,7 +132,7 @@ int main(){
     //Reto 5
     Message message1={"Hola Josue, reunion manana", SENT, "juan", 1, {"juan"}};
     Message message2={"Hola Jared, reunion manana", DELIVERED, "pepe", 2, {"josue", "mario"}};
-    Message message3={"Hola amor, ¿Que tal?", FAILED, "juan", 3, {"josue", "luis", "pedro"}};
+    Message message3={"Hola amor, Que tal?", FAILED, "juan", 3, {"josue", "luis", "pedro"}};
     Message message4={"Hola jochis, reunion manana", SENT, "ashton", 1, {"juan"}};
     Message message5={"Hola ashton, estas reprobado", SENT, "angel", 1, {"josue"}};
     Message message6={"Hola nila , ", FAILED, "ana", 1, {"josue"}};
